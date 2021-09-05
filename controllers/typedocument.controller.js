818abcacc721ffa7ebd443603e095eb8c2331f5d
NAKANY ANDRIAMIHAJA Michel <mnakanyandriamihaja@gmail.com>
2021-09-05 22:56:55 +0500

    Update typedocument.controller.js

--- controllers/typedocument.controller.js
+++ controllers/typedocument.controller.js
@@ -2,20 +2,14 @@ const routeService = require('../services/routes.service');
 const typeDocumentService = require('../services/typedocument.service');
 
 exports.getActiveTypes = async (req, res) => {
-    // let decodedToken;
-    // try {
-    //     decodedToken = routeService.verifyToken(req);
-    // } catch (err) {
-    //     return res.status(403).send(err);
-    // }
+    let decodedToken;
+    try {
+        decodedToken = routeService.verifyToken(req);
+    } catch (err) {
+        return res.status(403).send(err);
+    }
 
-    // const types = await typeDocumentService.getActiveTypes();
-    const types = [
-        {
-            "IDTYPEDOCUMENT" : "1",
-            "NOM TYPE" : "Memoire complet MC",
-            "STATUT_TYPE" : "ACTIVE"
-        }
-    ]
+    const types = await typeDocumentService.getActiveTypes();
+
     res.json(types);
 }
