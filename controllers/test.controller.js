0e4977c46e111ad32ff76dd255a42a651c80a563
NAKANY ANDRIAMIHAJA Michel <mnakanyandriamihaja@gmail.com>
2021-09-06 09:28:05 +0500

    Update test.controller.js

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -4,6 +4,7 @@ const config = require('../config/config');
 const memoireService = require('../services/memoire.service');
 const routeService = require('../services/routes.service');
 const logger = require('../helpers/logger');
+const { Client } = require('@elastic/elasticsearch')
 
 /**
  * Save Memoire API
@@ -11,9 +12,22 @@ const logger = require('../helpers/logger');
  * @param {Response} res Response
  */
 exports.hello = async (req, res) => {
-    let data = {
-        status : "ok",
-        test : "ok"
-    }
-    res.status(200).json(data);
+    const client = new Client({
+        cloud: {
+          id: "My_deployment:dXMtd2VzdDEuZ2NwLmNsb3VkLmVzLmlvJDNlYmZhMWI5MWIwNzQ4NDNhODE4ZDk2NTUzOWMwODgzJGRjMjQ2ZjQxMDZhNzQwYzg4YWUxNDI5MTI5N2Y0YThl"
+        },
+        auth: {
+          username: "elastic",
+          password: "nJpK2TvfZrNzChLliHMNyvFW"
+        }
+    })
+    const { body } = await client.search({
+        index: 'game-of-thrones',
+        body: {
+          query: {
+            match: { quote: 'winter' }
+          }
+        }
+    })
+    res.status(200).json(body.hits.hits);
 }
